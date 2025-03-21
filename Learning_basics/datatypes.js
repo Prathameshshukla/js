bf5f122feda4c1107e265237a39a808f41e4ca28
Prathmeshshukla <ps961853@gmail.com>
2025-03-21 11:53:50 +0530

    datatypes

---
+++ Learning_basics/datatypes.js
@@ -0,0 +1,18 @@
+let num=33;
+
+console.log(num)
+
+let str="Radhe Radhe";
+// console.log(str)
+
+let long=16849816555456461235n;
+console.log(long);
+
+let boole=true;
+console.log(boole)
+
+let dt=undefined;
+console.log(dt)
+
+let dt1=null
+console.log(dt1)
